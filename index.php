eea29d4a862b24e91f82939e7028179d69e0bfdf
olilinvar <olilinvar@gmail.com>
2023-12-01 11:46:10 +0100

    update

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+$servername = "192.168.234.11";
+$username = "localhost";
+$password = "password";
+$dbname = "mydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
